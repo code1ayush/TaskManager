d10f14fcf28f02cc10bc68c7c8395abfc407ab90
Ayush Kumar <ayushkumar@Ayushs-MacBook-Air.local>
2025-08-15 10:22:07 +0530

    added docker file

--- src/main/java/com/Ayush/TaskManager/config/SpringSecurity.java
+++ src/main/java/com/Ayush/TaskManager/config/SpringSecurity.java
@@ -21,7 +21,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@Profile("dev")
+//@Profile("dev")
 public class SpringSecurity {
 
     private final UserDetailServiceImp userDetailService;
--- src/main/java/com/Ayush/TaskManager/config/SpringSecurityProd.java
+++ src/main/java/com/Ayush/TaskManager/config/SpringSecurityProd.java
@@ -1,44 +1,44 @@
-package com.Ayush.TaskManager.config;
-
-import com.Ayush.TaskManager.Services.UserDetailServiceImp;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-@Profile("prod")
-public class SpringSecurityProd {
-
-    private final UserDetailServiceImp userDetailService;
-
-    public SpringSecurityProd(UserDetailServiceImp userDetailService) {
-        this.userDetailService = userDetailService;
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        return http.authorizeHttpRequests(request -> request
-                        .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
-                .build();
-    }
-
-
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
-
-//csrf = cross site request forgery
+//package com.Ayush.TaskManager.config;
+//
+//import com.Ayush.TaskManager.Services.UserDetailServiceImp;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//@Profile("prod")
+//public class SpringSecurityProd {
+//
+//    private final UserDetailServiceImp userDetailService;
+//
+//    public SpringSecurityProd(UserDetailServiceImp userDetailService) {
+//        this.userDetailService = userDetailService;
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//        return http.authorizeHttpRequests(request -> request
+//                        .anyRequest().authenticated())
+//                .httpBasic(Customizer.withDefaults())
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .build();
+//    }
+//
+//
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
+//
+////csrf = cross site request forgery
